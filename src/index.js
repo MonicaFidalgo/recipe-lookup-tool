c5fea73285e4fd91cc124ea938b1da8d93ad460a
Karen Olson <karmarie@gmail.com>
2021-08-18 16:48:00 -0500

    Cleaned up comments

--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
-// DOMContentLoaded event listener, call initialize
 document.addEventListener("DOMContentLoaded", initialize);
 
-//FUNCTION: Initialize
 function initialize(e) {
-  // Add an event listener to the dropdown
-  //      "change", (e) => handleDropdown(e)
   document
     .querySelector("#categories")
-    .addEventListener("change", (e) => handleDropdown(e));
+    .addEventListener("change", handleDropdown);
 }
 
-// FUNCTION: handleDropdown(e)
 function handleDropdown(e) {
-  // Get the value of the dropdown selection
-  // Make a fetch request to get items from the desired category
-  // .then(recipeData) => recipeData["meals"].forEach(renderOneRecipe))
   document.querySelector("#card-container").innerHTML = "";
 
   const category = e.target.value;
 
   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
     .then((resp) => resp.json())
-    .then((recipeData) => recipeData["meals"].forEach(renderOneRecipe));
+    .then((recipeData) => {
+      recipeData["meals"].forEach(renderOneRecipe);
+    });
 }
 
-// FUNCTION: renderOneRecipe(recipe)
 function renderOneRecipe(recipe) {
   const li = document.createElement("li");
   li.className = "card";
@@ -36,6 +29,7 @@ function renderOneRecipe(recipe) {
 
   const h3 = document.createElement("h3");
   h3.innerText = recipe["strMeal"];
+
   if (h3.innerText.length > 48) {
     h3.className = "overflowText";
   }
@@ -46,29 +40,26 @@ function renderOneRecipe(recipe) {
 
   const likeCounter = document.createElement("span");
 
-  // Get the current info in db.json and pass it to handleData
   fetch("http://localhost:3000/meals")
     .then((resp) => resp.json())
-    .then((data) => handleData(data, id, likeCounter));
+    .then((currentLikeData) =>
+      setLikeCounter(currentLikeData, id, likeCounter)
+    );
 
   const likeButton = document.createElement("button");
   likeButton.className = "like-btn";
   likeButton.innerText = "Like";
   likeButton.addEventListener("click", (e) => handleLike(e));
 
-  li.append(img, h3, id, likeCounter, likeButton);
+  li.append(img, h3, id);
   document.querySelector("#card-container").appendChild(li);
 }
 
-function handleData(data, id, likeCounter) {
-  // Iterate through the data to see if the current item's ID is in there
-  // use .find?
-  const likedMeal = data.find((element) => element["id"] === id.innerText);
+function setLikeCounter(currentLikeData, id, likeCounter) {
+  const likedMeal = currentLikeData.find(
+    (element) => element["id"] === id.innerText
+  );
 
-  // Based on the value of likedMeal, set the innerText of likeCounter
-  // If it doesn't exist in the DB, set likeCounter.innertText to "0 likes"
-  // Else if there's 1 like, set likeCounter.innerText to `${meal["likes"] like`
-  // Else, set likeCounter.innerText to `${meal["likes"] likes`
   if (typeof likedMeal === "undefined") {
     likeCounter.innerText = "0 likes";
   } else if (likedMeal["likes"] === 1) {
@@ -78,7 +69,6 @@ function handleData(data, id, likeCounter) {
   }
 }
 
-//  FUNCTION: handleLike(e)
 function handleLike(e) {
   let currentLikes = parseInt(e.target.previousElementSibling.innerText);
   const id = e.target.previousElementSibling.previousElementSibling.innerText;
